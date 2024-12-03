8ec3aa71d316b78171db173ec9e2f7d0ba103ca3
Sander <mail@sanderboer.nl>
2024-12-03 21:43:21 +0100

    remove dependency on etherport-client

--- packages/components/index.ts
+++ packages/components/index.ts
@@ -15,5 +15,4 @@ export * from './src/components/RGB';
 export * from './src/components/Sensor';
 export * from './src/components/Servo';
 export * from './src/types';
-export * from 'etherport-client';
 export * from './src/TcpSerial';
