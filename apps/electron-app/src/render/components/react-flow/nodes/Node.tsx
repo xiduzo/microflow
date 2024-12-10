a18aad2ac752ed1e99863602559b341be86985f1
Sander <mail@sanderboer.nl>
2024-12-10 22:10:32 +0100

    refactor(NewNodeProvider): moved from context to zustand store

--- apps/electron-app/src/render/components/react-flow/nodes/Node.tsx
+++ apps/electron-app/src/render/components/react-flow/nodes/Node.tsx
@@ -166,6 +166,7 @@ function NodeSettingsPane<T extends Record<string, unknown>>(
 	const updateNode = useUpdateNode(id);
 
 	const ref = useRef<HTMLDivElement>(null);
+	const autoUpdate = useRef<NodeJS.Timeout | null>(null);
 	const settings = useRef(data);
 	const handlesToDelete = useRef<string[]>([]);
 
@@ -180,6 +181,22 @@ function NodeSettingsPane<T extends Record<string, unknown>>(
 			title: `${data.label} (${id})`,
 			container: ref.current ?? undefined,
 		});
+
+		function saveSettings() {
+			deleteEdes(id, handlesToDelete.current);
+			updateNode(settings.current, type !== 'Note');
+			updateNodeInternals(id);
+		}
+
+		pane.on('change', event => {
+			if (!event.last) return;
+
+			autoUpdate.current && clearTimeout(autoUpdate.current);
+			autoUpdate.current = setTimeout(() => {
+				saveSettings();
+			}, 500);
+		});
+
 		pane.registerPlugin(TweakpaneEssentialPlugin);
 		pane.registerPlugin(TweakpaneTextareaPlugin);
 		pane.registerPlugin(TweakpaneCameraPlugin);
@@ -188,26 +205,12 @@ function NodeSettingsPane<T extends Record<string, unknown>>(
 			index: 9998,
 		});
 
-		function saveSettings() {
-			deleteEdes(id, handlesToDelete.current);
-			updateNode(settings.current, type !== 'Note');
-			updateNodeInternals(id);
-		}
-
 		pane
 			.addButton({
-				title: 'Save',
-				index: 9998,
-			})
-			.on('click', saveSettings);
-
-		pane
-			.addButton({
-				title: 'Save & Close',
+				title: 'Close',
 				index: 9999,
 			})
 			.on('click', () => {
-				saveSettings();
 				setSettingsOpened(false);
 			});
 
