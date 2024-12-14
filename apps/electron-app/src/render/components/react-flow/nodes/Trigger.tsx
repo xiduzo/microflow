65f3f15baab7d3cd5d42186b8414aac24af8b92d
boringrgb <boringrgb@nordzee>
2024-12-14 01:20:09 +0100

    added Trigger node

---
+++ apps/electron-app/src/render/components/react-flow/nodes/Trigger.tsx
@@ -0,0 +1,80 @@
+import type { TriggerData, TriggerValueType } from '@microflow/components';
+import { Position } from '@xyflow/react';
+import { useEffect } from 'react';
+import { Handle } from './Handle';
+import { BaseNode, NodeContainer, useNode, useNodeSettingsPane } from './Node';
+import { useNodeValue } from '../../../stores/node-data';
+
+const numberFormat = new Intl.NumberFormat();
+
+export function Trigger(props: Props) {
+	return (
+		<NodeContainer {...props}>
+			<Value />
+			<Settings />
+			<Handle type="target" position={Position.Left} id="signal" offset={-0.5} />
+			<Handle type="source" position={Position.Bottom} id="change" />
+		</NodeContainer>
+	);
+}
+
+function Value() {
+	const { id, data } = useNode();
+	const threshold = data['threshold']; //useNodeAttribute<TriggerData>(id, 'threshold', 0);
+	const behaviour = data['behaviour']; //useNodeAttribute<TriggerData>(id, 'behaviour', 0);
+
+	let comp: string = '\u2261';
+	switch (behaviour) {
+		case 'exact': {
+			comp = '\u2261'; // identical/exact match
+			break;
+		}
+		case 'increasing': {
+			comp = '\u003E'; // greater than
+			break;
+		}
+		case 'decreasing': {
+			comp = '\u003C'; // less than
+			break;
+		}
+	}
+
+	return (
+		<section className="tabular-nums">
+			{comp} {threshold}
+		</section>
+	); //{numberFormat.format(Math.round(value))}</section>;
+}
+
+function Settings() {
+	const { pane, settings } = useNodeSettingsPane<TriggerData>();
+
+	useEffect(() => {
+		if (!pane) return;
+
+		pane.addBinding(settings, 'behaviour', {
+			index: 0,
+			view: 'list',
+			label: 'behaviour',
+			options: [
+				{ value: 'increasing', text: 'when increasing' },
+				{ value: 'exact', text: 'when exactly equal' },
+				{ value: 'decreasing', text: 'when decreasing' },
+			],
+		});
+
+		pane.addBinding(settings, 'threshold', {
+			index: 1,
+			label: 'threshold value',
+		});
+	}, [pane, settings]);
+
+	return null;
+}
+
+type Props = BaseNode<TriggerData, TriggerValueType>;
+export const DEFAULT_TRIGGER_DATA: Props['data'] = {
+	label: 'Trigger',
+	behaviour: 'exact',
+	threshold: 0,
+};
---
+++ packages/components/src/components/Trigger.ts
@@ -0,0 +1,113 @@
+import { BaseComponent, BaseComponentOptions } from './BaseComponent';
+
+export type TriggerBehaviour = 'increasing' | 'exact' | 'decreasing';
+
+//export type WaveformType = 'sinus' | 'square' | 'sawtooth' | 'triangle' | 'random';
+
+export type TriggerData = {
+	behaviour: TriggerBehaviour;
+	threshold: number;
+};
+export type TriggerValueType = number;
+
+type TriggerOptions = BaseComponentOptions & TriggerData;
+
+export class Trigger extends BaseComponent<TriggerValueType> {
+	private previousValue: number = Number.NaN; // safe initial value
+	private firstDerivative: number = Number.NaN;
+	private thresholdCrossed: boolean = false;
+
+	constructor(private readonly options: TriggerOptions) {
+		super(options, 0);
+	}
+
+	checkIncreasing(value: number): boolean {
+		if (this.thresholdCrossed) {
+			this.firstDerivative = value - this.previousValue;
+
+			// is decreasing
+			if (this.firstDerivative < 0) {
+				this.thresholdCrossed = false; // reset when decreasing
+			}
+
+			this.previousValue = value;
+			return false;
+		}
+
+		if (this.previousValue !== Number.NaN) {
+			this.firstDerivative = value - this.previousValue;
+
+			// is increasing
+			if (this.firstDerivative > 0) {
+				if (value >= this.options.threshold) {
+					this.thresholdCrossed = true;
+				}
+			}
+		}
+
+		this.previousValue = value;
+		return this.thresholdCrossed;
+	}
+
+	checkDecreasing(value: number): boolean {
+		if (this.thresholdCrossed) {
+			this.firstDerivative = value - this.previousValue;
+
+			// is increasing
+			if (this.firstDerivative > 0) {
+				this.thresholdCrossed = false; // reset when decreasing
+			}
+
+			this.previousValue = value;
+			return false;
+		}
+
+		if (this.previousValue !== Number.NaN) {
+			this.firstDerivative = value - this.previousValue;
+
+			// is decreasing
+			if (this.firstDerivative < 0) {
+				if (value <= this.options.threshold) {
+					this.thresholdCrossed = true;
+				}
+			}
+		}
+
+		this.previousValue = value;
+		return this.thresholdCrossed;
+	}
+
+	signal(value: number) {
+		let retval: boolean = false;
+
+		// console.log(
+		// 	`style:  ${this.options.behaviour} / thresh: ${this.options.threshold} / fd: ${this.firstDerivative}  / prev: ${this.previousValue}  / crossed: ${this.thresholdCrossed}`,
+		// );
+
+		switch (this.options.behaviour) {
+			case 'increasing': {
+				retval = this.checkIncreasing(value);
+				//console.log(`[>] trigger ${this.value} < ${value}`);
+				break;
+			}
+			case 'exact': {
+				retval = this.value === value ? true : false;
+				//console.log(`[=] trigger  ${this.value} = ${value}`);
+				break;
+			}
+			case 'decreasing': {
+				retval = this.checkDecreasing(value); //this.value > value ? true : false;
+				//console.log(`[<] trigger  ${this.value} > ${value}`);
+				break;
+			}
+		}
+
+		if (retval) {
+			//console.log(`(!) was triggered`);
+			//this.value = value;
+			this.bang();
+		} else {
+			this.quiet();
+		}
+	} // input()
+} // Trigger component
