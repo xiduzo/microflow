873b5b6be0e8732932ffa7bc79c32f305eb51bdb
Sander <mail@sanderboer.nl>
2025-01-06 23:18:18 +0100

    chore: bump package

--- apps/nextjs-app/components/DownloadApp.tsx
+++ apps/nextjs-app/components/DownloadApp.tsx
@@ -23,7 +23,7 @@ export function DownloadApp() {
 	const [os, setOs] = useState<OperationSystem>();
 
 	function downloadApp() {
-		const version = '0.7.5';
+		const version = '0.7.6';
 		const baseUrl = `https://github.com/xiduzo/microflow/releases/download/v${version}`;
 
 		switch (os) {
