697cc56f1c63b0e5310a82792579d1cc06ba91d9
Sander <mail@sanderboer.nl>
2025-01-11 12:08:51 +0100

    chore(Handle): make it clearer what you connect and when it is possible

--- apps/electron-app/src/render/components/react-flow/nodes/Handle.tsx
+++ apps/electron-app/src/render/components/react-flow/nodes/Handle.tsx
@@ -1,4 +1,4 @@
-import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from '@microflow/ui';
+import { cva, Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from '@microflow/ui';
 import {
 	HandleProps,
 	Position,
@@ -20,9 +20,23 @@ export function Handle(props: Props) {
 			? props.isConnectable
 			: (props.isConnectable?.(edges) ?? true);
 	}, [props.isConnectable, edges]);
+
+	const tooltipSide = useMemo(() => {
+		switch (props.position) {
+			case Position.Top:
+				return 'bottom';
+			case Position.Bottom:
+				return 'top';
+			case Position.Left:
+				return 'right';
+			case Position.Right:
+				return 'left';
+		}
+	}, [props.position]);
+
 	return (
 		<TooltipProvider>
-			<Tooltip delayDuration={350}>
+			<Tooltip delayDuration={0}>
 				<TooltipTrigger asChild>
 					<XyFlowHandle
 						{...props}
@@ -36,6 +50,7 @@ export function Handle(props: Props) {
 							if (edge.source === edge.target) return false;
 							return true;
 						}}
+						className={handle({ className: props.className })}
 						style={{
 							width: [Position.Top, Position.Bottom].includes(props.position) ? 20 : 10,
 							height: [Position.Left, Position.Right].includes(props.position) ? 20 : 10,
@@ -47,9 +62,13 @@ export function Handle(props: Props) {
 								: 0,
 							...props.style,
 						}}
-					/>
+					>
+						<span className="mb-0.5 pointer-events-none">
+							{(props.title ?? props.id ?? '').slice(0, 1).toLowerCase()}
+						</span>
+					</XyFlowHandle>
 				</TooltipTrigger>
-				<TooltipContent className="text-center">
+				<TooltipContent className="text-center" side={tooltipSide}>
 					<p>{props.title ?? props.id}</p>
 					{props.hint && <p className="text-muted-foreground">{props.hint}</p>}
 				</TooltipContent>
@@ -64,3 +83,5 @@ type Props = Omit<HandleProps, 'isConnectable' | 'isValidConnection'> & {
 	isConnectable?: ((edges: Edge[]) => boolean) | boolean;
 	isValidConnection?: (edges: Edge[], edge: Edge | Connection) => boolean;
 };
+
+const handle = cva('text-xs flex items-center justify-center z-50');
