bdefabc75b835fca6296f5413c1f252bdc1d4ef1
Sander <mail@sanderboer.nl>
2025-01-12 14:48:23 +0100

    fix(Matrix): able to handle more devices

--- packages/components/src/components/Matrix.ts
+++ packages/components/src/components/Matrix.ts
@@ -28,18 +28,44 @@ export class Matrix extends BaseComponent<MatrixValueType> {
 	show(index: unknown) {
 		this.component.on();
 
+		// Find the shape at the rounded index
 		const shape = this.data.shapes.at(Math.round(transformValueToNumber(index) - 1));
 
 		if (!shape) {
 			return;
 		}
 
-		this.component.draw(0, shape as any as number);
+		const columnWidth = this.getColumnWidth();
+		const shapes = shape.reduce((acc, row) => {
+			// Split each row into shapes of the column width
+			const rowShapes = row.match(new RegExp(`.{1,${columnWidth}}`, 'g'));
+
+			if (!rowShapes) return acc;
+
+			// Push each shape into the correct index (device)
+			rowShapes.forEach((shape, index) => {
+				if (!acc[index]) {
+					acc[index] = [];
+				}
+				acc[index].push(shape);
+			});
+
+			return acc;
+		}, [] as MatrixShape[]);
+
+		// Show each shape on the matrix for each device
+		shapes.forEach((shape, index) => {
+			this.component.draw(index, shape as any as number);
+		});
 		this.value = shape;
 	}
 
 	hide() {
 		this.component.off();
 		this.value = this.value.map(row => row.replace(/'1'/g, '0'));
+	}
+
+	private getColumnWidth() {
+		return Number(this.data.dims.split('x').map(Number)[1]);
 	}
 }
