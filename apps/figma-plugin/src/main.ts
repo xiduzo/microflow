b09112c31e11383aebf27453fd075c1d1536aabf
xiduzo <mail@sanderboer.nl>
2026-03-16 21:05:42 +0100

    init plugin

---
+++ apps/figma-plugin/src/main.ts
@@ -0,0 +1,15 @@
+import { loadFontsAsync, once, showUI } from '@create-figma-plugin/utilities'
+
+import { type InsertCodeHandler } from './types'
+
+export default function () {
+  once<InsertCodeHandler>('INSERT_CODE', async function (code: string) {
+    const text = figma.createText()
+    await loadFontsAsync([text])
+    text.characters = code
+    figma.currentPage.selection = [text]
+    figma.viewport.scrollAndZoomIntoView([text])
+    figma.closePlugin()
+  })
+  showUI({ height: 232, width: 320 })
+}
